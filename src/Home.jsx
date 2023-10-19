77385567ad81e984cde9956546df6290bfb95040
GetDrogon <xiaofan8272@outlook.com>
2023-10-19 21:17:15 +0800

    UI

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,40 +6,31 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Header from "./Header";
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { SupportedChainIds } from "./utils/FChainDef";
 import FMintNFT from "./web3/FMintNFT";
 import "./Home.scss";
-const Home = () => {
-  const contractAddr = "0x1DE087B2Da4EC6F62c1917F39b89D1271E78fd05";
-  const injectedConnector = new InjectedConnector({
-    supportedChainIds: SupportedChainIds,
-  });
-  const { activate, account, chainId, active, library, deactivate } =
-    useWeb3React();
-
+const FNFTItem = (props) => {
+  const { addr, library } = props;
   const [nftInfo, setNFTInfo] = useState({
     name: "",
     description: "",
   });
 
   useEffect(() => {
-    addWeb3Listen();
-    return () => {
-      removeWeb3Listen();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (account) {
+    if (library) {
       getNFTName();
     }
-  }, [account]);
+  }, [library]);
 
   const getNFTName = () => {
-    FMintNFT.name(contractAddr, library)
+    FMintNFT.name(addr, library)
       .then((res) => {
         if (typeof res == "string" && res.length > 0) {
           nftInfo.name = res;
@@ -50,10 +41,73 @@ const Home = () => {
         console.log(err, "err");
       });
   };
+  return (
+    <ListItem disablePadding>
+      <Typography
+        sx={{
+          fontSize: "14px",
+          // fontFamily: "Saira",
+          fontWeight: "500",
+          textAlign: "center",
+          color: "black",
+        }}
+      >
+        {addr}
+      </Typography>
+      <Box sx={{ flexGrow: 1 }} />
+      <Typography
+        sx={{
+          fontSize: "14px",
+          // fontFamily: "Saira",
+          fontWeight: "500",
+          textAlign: "center",
+          color: "black",
+        }}
+      >
+        {nftInfo.name}
+      </Typography>
+      <Box sx={{ flexGrow: 1 }} />
+      <Button
+        sx={{
+          display: { xs: "none", md: "flex" },
+          fontFamily: "monospace",
+          fontWeight: 400,
+          fontSize: "1rem",
+          color: "rgba(54,61,80,1)",
+          textDecoration: "none",
+          borderRadius: ".6rem",
+          border: 1,
+          borderColor: "rgba(54,61,80,1)",
+        }}
+        onClick={(event) => {
+          event.stopPropagation();
+          props.onMintClick(addr);
+        }}
+      >
+        {"MINT"}
+      </Button>
+    </ListItem>
+  );
+};
+const Home = () => {
+  const contractAddrs = ["0x1DE087B2Da4EC6F62c1917F39b89D1271E78fd05"];
+  const contractAddr = "0x1DE087B2Da4EC6F62c1917F39b89D1271E78fd05";
+  const injectedConnector = new InjectedConnector({
+    supportedChainIds: SupportedChainIds,
+  });
+  const { activate, account, chainId, active, library, deactivate } =
+    useWeb3React();
 
-  const mint = () => {
-    if (account) {
-      FMintNFT.mint(contractAddr, library, account)
+  useEffect(() => {
+    addWeb3Listen();
+    return () => {
+      removeWeb3Listen();
+    };
+  }, []);
+
+  const mint = (addr) => {
+    if (account && addr.length > 0) {
+      FMintNFT.mint(addr, library, account)
         .then((res) => {
           console.log(res, "res");
         })
@@ -118,46 +172,43 @@ const Home = () => {
     }
   };
   return (
-    <Container disableGutters={true} maxWidth={false} sx={{
-      backgroundColor:"red",
-    }}>
-      <Header account={account} onConnectClick={()=>{
-        activateMask();
-      }} onDisconnectClick={()=>{
-        deactivate();
-      }} />
-      <Typography
+    <Container
+      disableGutters={true}
+      maxWidth={false}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
+      <Header
+        account={account}
+        onConnectClick={() => {
+          activateMask();
+        }}
+        onDisconnectClick={() => {
+          deactivate();
+        }}
+      />
+      <Box
         sx={{
-          fontSize: "14px",
-          // fontFamily: "Saira",
-          fontWeight: "500",
-          textAlign: "center",
-          color: "black",
+          width: "60rem",
+          marginTop:"2rem"
         }}
       >
-        {nftInfo.name}
-      </Typography>
-      <Button
-        variant="contained"
-        sx={{
-          marginTop: "42px",
-          width: "200px",
-          height: "48px",
-          fontSize: "16px",
-          fontFamily: "Saira",
-          fontWeight: "500",
-          borderRadius: "5px",
-          color: "#FFFFFF",
-        }}
-        onClick={(event) => {
-          event.stopPropagation();
-          mint();
-        }}
-      >
-        {"MINT"}
-      </Button>
+        <List>
+          {contractAddrs.map((addr, index) => {
+            return (
+              <FNFTItem key={addr + index} addr={addr} library={library} onMintClick={(addr)=>{
+                mint(addr);
+              }}/>
+            );
+          })}
+        </List>
+      </Box>
     </Container>
   );
-}
+};
 
 export default Home;
--- src/utils/FChainDef.js
+++ src/utils/FChainDef.js
@@ -1,9 +1,9 @@
 
 
 export const SupportedChainIds = [
-  56, //BSC
-  97, //BSCTEST
-  137,//MATIC
-  80001, //MATICTEST
+  // 56, //BSC
+  // 97, //BSCTEST
+  // 137,//MATIC
+  // 80001, //MATICTEST
   5000, //MANTLE
 ]
